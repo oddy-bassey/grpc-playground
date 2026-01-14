a6c448d5641455709bcddff056f0a066773022eb
oddy-bassey <oddybassey@gmail.com>
2026-01-14 20:13:46 +0100

    serialization and deserialization

---
+++ src/main/java/com/example/sec03/Lec02Serialization.java
@@ -0,0 +1,43 @@
+package com.example.sec03;
+
+import com.example.models.sec03.Person;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Lec02Serialization {
+    private static final Logger log = LoggerFactory.getLogger(Lec02Serialization.class);
+    private static final Path PATH = Path.of("person.ouput");
+
+    public static void main(String[] args) throws IOException {
+        var person = Person.newBuilder()
+                .setName("Abu")
+                .setAge(18)
+                .setEmail("abu@gmail.com")
+                .setEmployed(true)
+                .setSalary(500.43)
+                .setBankAccountNumber(1234567890L)
+                .setBalance(1000)
+                .build();
+
+        serialize(person);
+        log.info("{}", deserialize());
+        log.info("{}", person.equals(deserialize()));
+        log.info("bytes length {}", person.toByteArray().length);
+    }
+
+    public static void serialize(Person person) throws IOException {
+        try (var outputStream = Files.newOutputStream(PATH)) {
+            person.writeTo(outputStream);
+        }
+    }
+
+    public static Person deserialize() throws IOException {
+        try (var inputStream = Files.newInputStream(PATH)) {
+            return Person.parseFrom(inputStream);
+        }
+    }
+}
