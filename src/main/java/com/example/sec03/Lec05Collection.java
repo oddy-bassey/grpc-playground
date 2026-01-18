98d2c6b873095d2266c1c5c01024eb3a4c4f39f7
oddy-bassey <oddybassey@gmail.com>
2026-01-18 16:01:37 +0100

    sets

--- src/main/java/com/example/sec03/Lec05Collection.java
+++ src/main/java/com/example/sec03/Lec05Collection.java
@@ -5,6 +5,7 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.util.List;
+import java.util.Set;
 
 public class Lec05Collection {
     private static final Logger log = LoggerFactory.getLogger(Lec05Collection.class);
@@ -27,7 +28,7 @@ public class Lec05Collection {
 //                .addBooks(book1)
 //                .addBooks(book2)
 //                .addBooks(book3)
-                .addAllBooks(List.of(book1, book2, book3))
+                .addAllBooks(Set.of(book1, book2, book3))
                 .build();
 
         log.info("library = {}", library);
