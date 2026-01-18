792523009cb5be62cfbd74b347aa453dded0b3fc
oddy-bassey <oddybassey@gmail.com>
2026-01-18 13:01:53 +0100

    collections

---
+++ src/main/java/com/example/sec03/Lec05Collection.java
@@ -0,0 +1,35 @@
+package com.example.sec03;
+
+import com.example.models.sec03.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.List;
+
+public class Lec05Collection {
+    private static final Logger log = LoggerFactory.getLogger(Lec05Collection.class);
+
+    public static void main(String[] args)
+    {
+
+        // create books
+        var book1 = Book.newBuilder()
+                .setTitle("Harry potter - part 1")
+                .setAuthor("J.K. Rowling")
+                .setPublicationYear(1997)
+                .build();
+
+        var book2 = book1.toBuilder().setTitle("Harry potter - part 2").setPublicationYear(1998).build();
+        var book3 = book1.toBuilder().setTitle("Harry potter - part 3").setPublicationYear(1999).build();
+
+        var library = Library.newBuilder()
+                .setName("Harry Potter")
+//                .addBooks(book1)
+//                .addBooks(book2)
+//                .addBooks(book3)
+                .addAllBooks(List.of(book1, book2, book3))
+                .build();
+
+        log.info("library = {}", library);
+    }
+}
